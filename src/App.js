4da915dce39a187a111cedb376e291f5820c0d8c
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-06-15 11:55:54 -0500

    chapter 13 props I

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,12 +6,42 @@ function Logo() {
   return <img src={logo} className="App-logo" alt="logo" />
 }
 
-function Hello(props) {
-  return(
-    <p>
-      {props.title}
-    </p>
+//FUNCTION
+// function Hello(props) {
+//   return(
+//     <p>
+//       {props.title}
+//     </p>
+//   )
+// }
+
+// ARROW FUNTCIONS
+// const Hello = (props) => <p>{props.title}</p>
+
+class Hello extends Component {
+  render() {
+    return <h1>{this.props.title}</h1>
+  }
+}
+
+class Text extends Component {
+  render() {
+    const textBool = this.props.isActivated ? 'On' : 'Off'
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n * 2)
+
+  return (
+    <div>
+      <p>{this.props.text}</p>
+      <p>{this.props.number}</p>
+      <p>{textBool}</p>
+      <p>{this.props.arrayOfNumbers.join(', ')}</p>
+      <p>{mappedNumbers.join(', ')}</p>
+      <p>{this.props.objectWithInfo.key}</p>
+
+      {/* <p>{JSON.stringify(this.props.boolean)}</p> */}
+    </div>
   )
+  }
 }
 
 function App() {
@@ -20,15 +50,13 @@ function App() {
       <header className="App-header">
         <Logo />
         <Hello title='Hello word'/>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Text
+          arrayOfNumbers={[2, 3, 10]}
+          objectWithInfo={{ key: 'value', key2: 'othervValue'}}
+          isActivated
+          number={2*2}
+          text='This is the component text'
+        />
       </header>
     </div>
   );
