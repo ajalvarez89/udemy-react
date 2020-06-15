ee021cba1fa7c6c209f2193423e73c296c24101a
Alvaro Alvarez <alvaro.alvarez@monokera.com>
2020-06-15 09:41:20 -0500

    setting the first component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Logo (props) {
+  return <img src={logo} className="App-logo" alt="logo" />
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
